731fc3f74c9fb7baa7133536419d798ea0627cd4
Nicole Chen <nicole_chen@college.harvard.edu>
2025-07-24 17:21:20 -0600

    misc

--- my-app/app/components/introduction.tsx
+++ my-app/app/components/introduction.tsx
@@ -6,7 +6,7 @@ import { Subscribe } from "./subscribe";
 export function Introduction(){
     return (
         <div>
-            <p className="text-neutral-600 mb-6"> the marketplace-turned-newsletter for you to shop for today&#39;s breakout, high-growth startups all in our portfolio </p> 
+            <p className="text-neutral-600 mb-6"> the marketplace-turned-newsletter for you to shop for today&#39;s highest-growth startups </p> 
             <p className="text-neutral-600 mb-6"> every week, we break down one company - their product, marketing, GTM  - to actually understand their trajectory and <strong>connect you with the founding team to see if there is mutual fit</strong> </p> 
             <p className="text-neutral-600 mb-6"><em>a cohort of 100 students hand-picked to launch the niche</em></p> 
             <Subscribe />
